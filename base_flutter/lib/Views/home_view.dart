1e8e5fe6d05b8f80e6fabbbd437886df3dc24a8c
ronny-sugianto <ronnysugianto.id@gmail.com>
2020-05-08 19:24:50 +0700

    [add] add project for fetch api with http

--- base_flutter/lib/Views/home_view.dart
+++ base_flutter/lib/Views/home_view.dart
@@ -1,13 +1,125 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomeView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeView extends StatefulWidget {
   static const ROUTE_URL = '/home';
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  var bookName = TextEditingController();
+  List listBook;
+  bool isLoading = false;
+
+  Future<void> getData() async {
+    print(bookName.text.toString());
+    var url = 'https://www.googleapis.com/books/v1/volumes?q=${bookName.text}';
+    print(url);
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    setState(() {
+      isLoading = true;
+    });
+    Future.delayed(Duration(milliseconds: 500), () {});
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+      var items = jsonResponse['items'];
+      isLoading = false;
+      setState(() {
+        listBook = items;
+      });
+      print(items.runtimeType);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Anda Sudah Login'),
+      appBar: AppBar(
+        title: Text('Flutter Fetch'),
+        centerTitle: true,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * 0.8,
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      flex: 4,
+                      child: TextField(
+                        controller: bookName,
+                        decoration: InputDecoration.collapsed(
+                            hintText: 'Search Book...'),
+                      )),
+                  Expanded(
+                    flex: 2,
+                    child: RaisedButton(
+                      elevation: 5,
+                      onPressed: getData,
+                      child: Text('Search'),
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              child: Container(
+                width: double.infinity,
+                color: Colors.blueGrey,
+                child: isLoading
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : listBook != null || listBook.toString() == '[]'
+                        ? ListView.builder(
+                            itemBuilder: (context, index) {
+                              return Card(
+                                child: ListTile(
+                                  leading: Image.network(listBook[index]
+                                                  ['volumeInfo']['imageLinks']
+                                              ['thumbnail']
+                                          .isEmpty
+                                      ? 'https://upload.wikimedia.org/wikipedia/commons/0/0a/No-image-available.png'
+                                      : listBook[index]['volumeInfo']
+                                          ['imageLinks']['thumbnail']),
+                                  title: Text(listBook[index]['volumeInfo']
+                                          ['title'] ??
+                                      'null'),
+                                  subtitle: Text(listBook[index]['volumeInfo']
+                                          ['publisher'] ??
+                                      'null'),
+                                ),
+                              );
+                            },
+                            itemCount: listBook == null ? 0 : listBook.length,
+                          )
+                        : Center(
+                            child: Text(
+                              'No Data Found',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- base_flutter/lib/main.dart
+++ base_flutter/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/Views/home_view.dart';
-import 'package:flutterapp/Views/login_view.dart';
 
 void main() => runApp(myApp());
 
@@ -8,11 +7,8 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: LoginView.ROUTE_URL,
-      routes: {
-        LoginView.ROUTE_URL: (_) => LoginView(),
-        HomeView.ROUTE_URL: (_) => HomeView()
-      },
+      initialRoute: HomeView.ROUTE_URL,
+      routes: {HomeView.ROUTE_URL: (_) => HomeView()},
     );
   }
 }
